13c6012d38b5b53cc3a04242b45f29d92580c7ed
Habiba Osama <habebaosama14@gmail.com>
2024-05-06 19:40:25 +0300

    initial

---
+++ DataAcquisition/weatherStation/src/main/java/Weather.java
@@ -0,0 +1,17 @@
+
+
+import lombok.Getter;
+import org.json.JSONArray;
+
+@Getter
+public class Weather {
+    private final JSONArray temperature;
+    private final JSONArray humidity;
+    private final JSONArray windSpeed;
+
+    public Weather(JSONArray temperature, JSONArray humidity, JSONArray windSpeed) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.windSpeed = windSpeed;
+    }
+}
