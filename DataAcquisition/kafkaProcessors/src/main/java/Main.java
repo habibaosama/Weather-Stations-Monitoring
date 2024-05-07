90049c50171dff3f8367abb4e25b40c939ad29d5
MaiAhmedHussein <mayoyaahmed66@gmail.com>
2024-05-08 00:08:01 +0300

    Raining Trigger Parsing Fixed :tada:

--- DataAcquisition/kafkaProcessors/src/main/java/Main.java
+++ DataAcquisition/kafkaProcessors/src/main/java/Main.java
@@ -3,6 +3,5 @@ public class Main {
     public static void main(String[] args) {
         RainingTriggerKafkaProcessor processor = new RainingTriggerKafkaProcessor();
         processor.process();
-        System.out.println("Kafka Streams application finished.");
     }
 }
